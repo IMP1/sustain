16e9a89a1ce233a65edc49f4f2e557a117e251c5
IMP1 <huw_taylor@hotmail.co.uk>
2023-01-13 16:57:23 +0000

    Work on river flow shader stuff

--- objects/River.gd
+++ objects/River.gd
@@ -2,6 +2,7 @@ extends Area2D
 class_name River
 
 export(float) var flow_speed: float = 128.0
+export(Texture) var wave_texture: Texture
 
 onready var _polygon: CollisionPolygon2D = $Polygon2D as CollisionPolygon2D
 onready var _path: Path2D = $Path as Path2D
@@ -12,11 +13,8 @@ onready var _bar: ProgressBar = $ProgressBar as ProgressBar
 var is_generated: bool = false
 
 func _ready():
-	# TODO: Create a water shader
-	#       Have it discard pixels that aren't over a waterway (using the blue color channel? or transparent on a layer?)
-	#       Give it a speed, and wave thickness, and wave colour
 	_bar.visible = false
-	print(_river_flow_data_path())
+	_flow_texture.visible = false
 	if not ResourceLoader.exists(_river_flow_data_path()):
 		var state = _generate_flow()
 		if state is GDScriptFunctionState:
@@ -26,6 +24,8 @@ func _ready():
 	_flow_texture.position = flowmap.position
 	_flow_texture.material.set_shader_param("scale", flowmap.scale)
 	_flow_texture.material.set_shader_param("flow_speed", flow_speed)
+	_flow_texture.material.set_shader_param("wave_texture", wave_texture)
+	_flow_texture.visible = true
 
 func _river_flow_data_path() -> String:
 	return "res://data/river_flowmaps/" + str(get_path()).replace("/", "_") + ".tres"
@@ -67,7 +67,7 @@ func _generate_flow() -> void:
 	_bar.max_value = height
 	for j in height:
 		for i in width:
-			_compute_flow_at_pixel(i, j, flow_data, offset_x, offset_y, width, height)
+			_compute_flow_at_pixel(i, j, flow_data, offset_x, offset_y)
 		yield(get_tree(), "idle_frame")
 		_bar.value = j
 	_bar.value = height
@@ -83,28 +83,39 @@ func _generate_flow() -> void:
 	_flow_texture.position = bounds.position
 	_bar.visible = false
 
-func _compute_flow_at_pixel(x: int, y: int, image: Image, ox:=0, oy:=0, width:=0, height:=0) -> void:
+func _compute_flow_at_pixel(x: int, y: int, image: Image, ox:=0, oy:=0) -> void:
 	if not Geometry.is_point_in_polygon(Vector2(x + ox, y + oy), _polygon.polygon):
 		return
-	var velocity := _flow_direction_at_pixel(Vector2(x + ox, y + oy))
-	var r: float = (clamp(velocity.x / width, -1, 1) + 1) / 2
-	var g: float = (clamp(velocity.y / height, -1, 1) + 1) / 2
+	var velocity := _flow_direction_at_pixel(Vector2(x, y)).normalized()
+	var r: float = (velocity.x + 1) / 2
+	var g: float = (velocity.y + 1) / 2
 	var color : Color = Color(r, g, 0.5, 1)
 	image.set_pixel(x, y, color)
 
 func _flow_direction_at_pixel(pos: Vector2) -> Vector2:
-	# TODO: I don't think this is working. I think it's a positional thing. Some are local, some are global. Maybe?
-	var nearest_path_point := _path.curve.get_closest_point(_path.to_local(pos))
-	var previous: Vector2 = Vector2.ZERO
-	var current: Vector2 = Vector2.ZERO
-	var next: Vector2 = pos
-	for point in _path.curve.get_baked_points():
-		previous = current
-		current = next
-		next = point
-		if current == nearest_path_point:
-			break
-	if current == nearest_path_point:
-		return next - current
-	else:
-		return current - previous
+	var flow_path_offset := 10
+	var nearest_path_offset := _path.curve.get_closest_offset(pos)
+	var nearest_path_point := _path.curve.interpolate_baked(nearest_path_offset)
+	var previous_offset := max(0, nearest_path_offset - flow_path_offset)
+	var next_offset := min(nearest_path_offset + flow_path_offset, _path.curve.get_baked_length())
+	var previous_point := _path.curve.interpolate_baked(previous_offset)
+	var next_point := _path.curve.interpolate_baked(next_offset)
+	var flow_direction := next_point - previous_point
+	var flow_offset := nearest_path_point - pos
+	return flow_direction + flow_offset / 2
+
+# DEBUGGING
+# TODO: Remove when river is working
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		var local_pos := get_local_mouse_position()
+		var dir := _flow_direction_at_pixel(local_pos)
+		var line := Line2D.new()
+		add_child(line)
+		line.add_point(local_pos)
+		line.add_point(local_pos + dir.normalized() * 48)
+		line.default_color = Color.white
+		line.width_curve = Curve.new()
+		line.width_curve.add_point(Vector2(0, 1))
+		line.width_curve.add_point(Vector2(1, 0))
+
